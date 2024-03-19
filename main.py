894bdfe56483fe61c79c62521d1b4ad36cd5516c
LUO WEN SHIN <wadevs14161@gmail.com>
2024-03-19 12:56:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,9 +103,10 @@ def find_product():
             line_bot_api.reply_message(
                 ReplyMessageRequest(
                     replyToken=event.reply_token,
-                    messages=[TextMessage(text=result[0])]
+                    messages=[TextMessage(text=result[1])]
                 )
             )
+            print("Sould print url")
             # line_bot_api.reply_message_with_http_info(
             #     ReplyMessageRequest(
             #         reply_token=event.reply_token,
